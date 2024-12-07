59c0fece466e29d7c7422bb9fc0b61e9d0d747f7
paul obiero <paulobiero147@gmail.com>
2024-12-07 23:18:04 +0300

    First commit

--- service.js
+++ service.js
@@ -9,138 +9,154 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/print-receipt', async (req, res) => {
-    const {company, imagePath, sale_no, customer, served_by, date,items,total,discount,tax,payment_method,pin} = req.body;
+    const {
+        company,
+        imagePath,
+        sale_no,
+        customer,
+        served_by,
+        date,
+        items,
+        total,
+        discount,
+        tax,
+        payment_method,
+        pin
+    } = req.body;
     console.log(req.body)
-   try {
-       const device = new escpos.USB();
-       const printer = new escpos.Printer(device);
-       device.open(async () => {
-           try {
-               if (imagePath) {
-                   const image = await loadImage(imagePath);
-                   printer.image(image);
-               }
-               printer.text("\n");
-               printer.align('ct').style('bold')
-                   .size(1, 1).text(company.name);
+    try {
+        const device = new escpos.USB();
+        const printer = new escpos.Printer(device);
+        device.open(async () => {
+            try {
+                if (imagePath) {
+                    const image = await loadImage(imagePath);
+                    printer.image(image);
+                }
+                printer.align('ct')
+                    .size(1, 1).text(company.name);
 
-               printer.align('ct').style('normal').size(0, 0).text(company.email.toUpperCase());
+                printer.encode('GB18030').align('ct').size(0, 0).text(company.email.toUpperCase());
 
-               printer.align('ct').style('normal').size(0, 0).text(`TEL:${company.phone}`);
-               printer.align('ct').style('bold')
-                   .size(1, 0).text('SALE RECEIPT');
-               printer.align('ct').style('normal').size(0, 0).text(`(COPY)`);
-               printer.align('lt').text("\n");
-               printer.style('bold').size(0, 0).text(`Sale No   : ${sale_no}`);
-               printer.style('bold').size(0, 0).text(`Served by :${served_by}`);
-               printer.style('bold').size(0, 0).text(`Customer  :${customer}`);
-               printer.style('bold').size(0, 0).text(`Date      :${date}`);
-               printer.align('ct').style('normal').size(0, 0).text(`------------------------------------------------`);
-               printer.tableCustom(
-                   [
-                       {text: "Item", align: "LEFT", width: 0.6, style: 'B'},
-                       {text: "Tax", align: "CENTER", width: 0.2, style: 'B'},
-                       {text: "Total", align: "RIGHT", width: 0.2, style: 'B'}
-                   ]
-               );
+                printer.encode('GB18030').align('ct').size(0, 0).text(`TEL:${company.phone}`);
+                printer.encode('GB18030').align('ct')
+                    .size(1, 0).text('SALE RECEIPT');
+                printer.encode('GB18030').align('ct').size(0, 0).text(`(COPY)`);
+                printer.encode('GB18030').align('lt').text("\n");
+                printer.encode('GB18030').size(0, 0).text(`Sale No   : ${sale_no}`);
+                printer.encode('GB18030').size(0, 0).text(`Served by :${served_by}`);
+                printer.encode('GB18030').size(0, 0).text(`Customer  :${customer}`);
+                printer.encode('GB18030').size(0, 0).text(`Date      :${date}`);
+                printer.encode('GB18030').align('ct').size(0, 0).text(`------------------------------------------------`);
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "Item", align: "LEFT", width: 0.6},
+                        {text: "Tax", align: "CENTER", width: 0.2},
+                        {text: "Total", align: "RIGHT", width: 0.2}
+                    ]
+                );
 
-               for (let i = 0; i < items.length; i++) {
-                   let item=items[i];
-                   printer.tableCustom(
-                       [
-                           {text: item.name, align: "LEFT", width: 0.6, style: 'N'},
-                           {text: "", align: "CENTER", width: 0.2, style: 'N'},
-                           {text: "", align: "RIGHT", width: 0.2, style: 'N'}
-                       ] // Optional
-                   );
-                   printer.tableCustom(
-                       [
-                           {text: `${item.quantity} ${item.unit_of_quantity} x ${item.unit_price}`, align: "LEFT", width: 0.6, style: 'N'},
-                           {text: `${item.tax}`, align: "CENTER", width: 0.2, style: 'N'},
-                           {text: `${item.price}`, align: "RIGHT", width: 0.2, style: 'N'}
-                       ]
-                   );
+                for (let i = 0; i < items.length; i++) {
+                    let item = items[i];
+                    printer.encode('GB18030').tableCustom(
+                        [
+                            {text: item.name, align: "LEFT", width: 0.6},
+                            {text: "", align: "CENTER", width: 0.2},
+                            {text: "", align: "RIGHT", width: 0.2}
+                        ] // Optional
+                    );
+                    printer.encode('GB18030').tableCustom(
+                        [
+                            {
+                                text: `${item.quantity} ${item.unit_of_quantity} x ${item.unit_price}`,
+                                align: "LEFT",
+                                width: 0.6
+                            },
+                            {text: `${item.tax}`, align: "CENTER", width: 0.2},
+                            {text: `${item.price}`, align: "RIGHT", width: 0.2}
+                        ]
+                    );
+                    printer.encode('GB18030').align('lt').text("\n");
+                }
+                printer.encode('GB18030').align('ct').size(0, 0).text(`------------------------------------------------`);
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "SUBTOTAL", align: "LEFT", width: 0.5},
+                        {text: `${total}`, align: "RIGHT", width: 0.5}
+                    ]
+                );
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "DISCOUNT", align: "LEFT", width: 0.5},
+                        {text: `${discount ?? 0}`, align: "RIGHT", width: 0.5,}
+                    ]
+                );
 
-               }
-               printer.align('ct').style('normal').size(0, 0).text(`------------------------------------------------`);
-               printer.tableCustom(
-                   [
-                       {text: "SUBTOTAL", align: "LEFT", width: 0.5, style: 'N'},
-                       {text: `${total}`, align: "RIGHT", width: 0.5, style: 'N'}
-                   ]
-               );
-               printer.tableCustom(
-                   [
-                       {text: "DISCOUNT", align: "LEFT", width: 0.5, style: 'N'},
-                       {text: `${discount??0}`, align: "RIGHT", width: 0.5, style: 'N'}
-                   ]
-               );
+                printer.encode('GB18030').align('ct').size(0, 0).text(`------------------------------------------------`);
 
-               printer.align('ct').style('normal').size(0, 0).text(`------------------------------------------------`);
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "Code", align: "LEFT", width: 0.25},
+                        {text: "Rate", align: "LEFT", width: 0.25},
+                        {text: "Taxable", align: "LEFT", width: 0.25},
+                        {text: "Tax Amount", align: "LEFT", width: 0.25},
+                    ]
+                );
 
-               printer.tableCustom(
-                   [
-                       {text: "Code", align: "LEFT", width: 0.25, style: 'B'},
-                       {text: "Rate", align: "LEFT", width: 0.25, style: 'B'},
-                       {text: "Taxable", align: "LEFT", width: 0.25, style: 'B'},
-                       {text: "Tax Amount", align: "LEFT", width: 0.25, style: 'B'},
-                   ]
-               );
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "VAT", align: "LEFT", width: 0.25},
+                        {text: "16", align: "LEFT", width: 0.25},
+                        {text: `${total ?? 0}`, align: "LEFT", width: 0.25},
+                        {text: `${tax ?? 0}`, align: "LEFT", width: 0.25},
+                    ] // Optional
+                );
 
-               printer.tableCustom(
-                   [
-                       {text: "VAT", align: "LEFT", width: 0.25, style: 'B'},
-                       {text: "16", align: "LEFT", width: 0.25, style: 'B'},
-                       {text: `${total??0}`, align: "LEFT", width: 0.25, style: 'B'},
-                       {text: `${tax??0}`, align: "LEFT", width: 0.25, style: 'B'},
-                   ] // Optional
-               );
+                printer.encode('GB18030').align('ct').size(0, 0).text(`------------------------------------------------`);
 
-               printer.align('ct').style('normal').size(0, 0).text(`------------------------------------------------`);
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "TOTAL(KES)", align: "LEFT", width: 0.5},
+                        {text: `${total}`, align: "RIGHT", width: 0.5}
+                    ]
+                );
 
-               printer.tableCustom(
-                   [
-                       {text: "TOTAL(KES)", align: "LEFT", width: 0.5, style: 'B'},
-                       {text: `${total}`, align: "RIGHT", width: 0.5, style: 'N'}
-                   ]
-               );
+                printer.encode('GB18030').align('ct').size(0, 0).text(`------------------------------------------------`);
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "PAYMENT METHOD", align: "LEFT", width: 0.5},
+                        {text: `${payment_method}`, align: "RIGHT", width: 0.5}
+                    ]
+                );
 
-               printer.align('ct').style('normal').size(0, 0).text(`------------------------------------------------`);
-               printer.tableCustom(
-                   [
-                       {text: "PAYMENT METHOD", align: "LEFT", width: 0.5, style: 'B'},
-                       {text: `${payment_method}`, align: "RIGHT", width: 0.5, style: 'N'}
-                   ]
-               );
+                printer.encode('GB18030').align('ct').size(0, 0).text(`------------------------------------------------`);
+                printer.encode('GB18030').tableCustom(
+                    [
+                        {text: "PIN", align: "LEFT", width: 0.5},
+                        {text: `${pin}`, align: "RIGHT", width: 0.5}
+                    ]
+                );
+                printer.encode('GB18030').align('ct').size(0, 0).text(`------------------------------------------------`);
 
-               printer.align('ct').style('normal').size(0, 0).text(`------------------------------------------------`);
-               printer.tableCustom(
-                   [
-                       {text: "PIN", align: "LEFT", width: 0.5, style: 'B'},
-                       {text: `${pin}`, align: "RIGHT", width: 0.5, style: 'N'}
-                   ]
-               );
-               printer.align('ct').style('normal').size(0, 0).text(`------------------------------------------------`);
-
-               printer.text("Powered by PEAKUNIFY");
-               printer.align('ct').style('normal').size(0, 0).text(`\n`);
-               printer.align('ct').style('normal').size(0, 0).text(`\n`).cut().close();
-               res.status(200).send({
-                   msg:'Print job completed'
-               });
-           } catch (error) {
-               console.log('Error printing:', error);
-               res.status(500).send({
-                   msg:'Print job failed'
-               });
-           }
-       });
-   }catch (e) {
-       console.log(e)
-       res.status(500).send({
-           msg:'Print job failed'
-       });
-   }
+                printer.encode('GB18030').text("Powered by PEAKUNIFY");
+                printer.encode('GB18030').align('ct').size(0, 0).text(`\n`);
+                printer.encode('GB18030').align('ct').size(0, 0).text(`\n`).cut().close();
+                res.status(200).send({
+                    msg: 'Print job completed'
+                });
+            } catch (error) {
+                console.log('Error printing:', error);
+                res.status(500).send({
+                    msg: 'Print job failed'
+                });
+            }
+        });
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({
+            msg: 'Print job failed'
+        });
+    }
 });
 
 function loadImage(filePath) {
--- windows-service.js
+++ windows-service.js
@@ -1,26 +1,34 @@
 const Service = require('node-windows').Service;
+var EventLogger = require('node-windows').EventLogger;
 
+var log = new EventLogger('PeakUnifyPrinter');
 // Define the service
-const svc = new Service({
-    name: 'PeakUnifyPrinter',
-    description: 'Peak unify printer service',
-    script: 'service.js',
-    execPath: process.execPath, // Ensure it uses the current Node.js binary
-    env: {
-        name: 'NODE_ENV',
-        value: 'production',
-    }
-});
+try {
+    const svc = new Service({
+        name: 'PeakUnifyPrinter',
+        description: 'Peak unify printer service',
+        script: 'service.js',
+        execPath: process.execPath, // Ensure it uses the current Node.js binary
+        env: {
+            name: 'NODE_ENV',
+            value: 'production',
+        }
+    });
 
 // Listen for the "install" event to start the service after installation
-svc.on('install', () => {
-    console.log('Service installed');
-    svc.start();
-});
+    svc.on('install', () => {
+        console.log('Service installed');
+        log.info('Service installed')
+        svc.start();
+    });
 
 // Check if the service is already installed
-if (!svc.exists) {
-    svc.install(); // Install the service
-} else {
-    console.log('Service already exists');
+    if (!svc.exists) {
+        svc.install(); // Install the service
+    } else {
+        console.log('Service already exists');
+        log.info('Service already exists')
+    }
+}catch (e) {
+    log.error(e);
 }
